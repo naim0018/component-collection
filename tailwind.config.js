6403e46711ed1abe2824d022b52fdf90851d87a3
Md Kazi Naim <119398884+naim0018@users.noreply.github.com>
2026-09-18 23:34:39 +0600

    Remove injected malware payload and artifacts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,3 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -12,4 +8,4 @@ export default {
     extend: {},
   },
   plugins: [],
-};  
+}
